8da0e6faa273640c0b7b837ee1d2214651304110
anvika yendela <anvika.yendela@atmecs.com>
2021-11-24 20:27:25 +0530

    updating changes

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -9,12 +9,6 @@ public class Addition {
     public static void main(String args[])
     {
     Addition a=new Addition();
-<<<<<<< HEAD
-    a.add(50,50);
-=======
-
-    a.add(100,100);
-
->>>>>>> 2d3589d8a58aa87c5d5bd6eadba0393066d7c05b
+a.add(65,65);
     }
 }
