f7b72814690af0ccb6cae0b5562aa5ca9b61214a
anvika yendela <anvika.yendela@atmecs.com>
2021-11-24 19:09:33 +0530

    upating add method

---
+++ src/main/java/Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+
+    public void add(int a,int b)
+    {
+        int c=a+b;
+        System.out.println("addition:"+c);
+    }
+
+    public static void main(String args[])
+    {
+    Addition a=new Addition();
+    a.add(10,20);
+    }
+}
