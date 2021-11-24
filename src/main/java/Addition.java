7ffdf080c6633fab4d25c75319e72c87f06fc515
AnvikaYendela <94821632+AnvikaYendela@users.noreply.github.com>
2021-11-24 19:31:14 +0530

    commiting add method

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -9,10 +9,8 @@ public class Addition {
     public static void main(String args[])
     {
     Addition a=new Addition();
-<<<<<<< HEAD
+
     a.add(20,20);
-=======
-    a.add(40,20);
->>>>>>> 1cc507bf5f76a4dae2088869f6b653d5f8e35875
+
     }
 }
